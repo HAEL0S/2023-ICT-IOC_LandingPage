83eb948f0984979b951165ab0bb97eb51f08d29c
상현 김 <thwj@me.com>
2022-01-14 21:10:04 +0900

    행사 시작 일시 변경합니다.

--- assets/js/main.js
+++ assets/js/main.js
@@ -93,7 +93,7 @@ var spy = new Gumshoe('#navigation a', {
 
 /* ======= Countdown ========= */
 // set the date we're counting down to
-var target_date = new Date("Jan 28, 2022").getTime();
+var target_date = new Date("Jan 21, 2022").getTime();
  
 // variables for time units
 var days, hours, minutes, seconds;
